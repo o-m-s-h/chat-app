5973f2f6ff93daa887ff6960e0c1b3248238cedb
Omkar M Shewalkar <omkar.shewalkar936@gmail.com>
2026-03-24 19:15:57 +0530

    Added auth routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const authRoutes = require("./modules/auth/auth.routes");
 
 const app = express();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 // Health check route
 app.get("/health", (req, res) => {
