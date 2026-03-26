41d2b628d532e8b9dba962a1e155d55309b6674b
Omkar M Shewalkar <omkar.shewalkar936@gmail.com>
2026-03-26 09:17:42 +0530

    chatRoutes

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const authRoutes = require("./modules/auth/auth.routes");
 const chatRoutes = require("./modules/chat/chat.routes");
 
-
 const app = express();
 
 // Middlewares
