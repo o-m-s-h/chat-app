bc2a424d35ae50e36d9331967d9c2a0422c916fd
Omkar M Shewalkar <omkar.shewalkar936@gmail.com>
2026-03-26 09:14:25 +0530

    TYPING, STOP_TYPING and MESSAGE_SEEN

--- server/src/modules/socket/events.js
+++ server/src/modules/socket/events.js
@@ -4,5 +4,8 @@ module.exports = {
   USER_ONLINE: "user_online",
   USER_OFFLINE: "user_offline",
   SEND_MESSAGE: "send_message",
-  RECEIVE_MESSAGE: "receive_message"
+  RECEIVE_MESSAGE: "receive_message",
+  TYPING: "typing",
+  STOP_TYPING: "stop_typing",
+  MESSAGE_SEEN: "message_seen"
 };
