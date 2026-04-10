6ae1c579336a159b2abb11ca59663e7fcc8f6b05
Omkar M Shewalkar <omkar.shewalkar936@gmail.com>
2026-04-10 14:34:47 +0530

    Added code to redirect through protected route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Chat from "./pages/Chat";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/chat"
+          element={
+            <ProtectedRoute>
+              <Chat />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/chat"
+          element={
+            <ProtectedRoute>
+              <Chat />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
