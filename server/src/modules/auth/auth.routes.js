e97d0795fd71ab1385502a639f9a90a556941594
Omkar M Shewalkar <omkar.shewalkar936@gmail.com>
2026-04-14 13:57:07 +0530

    Error fixed at auth.routes.js

--- server/src/modules/auth/auth.routes.js
+++ server/src/modules/auth/auth.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { register, login } = require("./auth.controller");
 const { authLimiter } = require("../../middleware/rateLimiter.middleware");
 
-router.post("/login", authLimiter, loginController);
-router.post("/register", authLimiter, registerController);
+router.post("/login", authLimiter, register);
+router.post("/register", authLimiter, login);
 
 module.exports = router;
