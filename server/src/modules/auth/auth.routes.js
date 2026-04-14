d8c257e7bf933165d3ddde835375aca746ba2a4a
Omkar M Shewalkar <omkar.shewalkar936@gmail.com>
2026-04-14 14:01:05 +0530

    login <---> register

--- server/src/modules/auth/auth.routes.js
+++ server/src/modules/auth/auth.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { register, login } = require("./auth.controller");
 const { authLimiter } = require("../../middleware/rateLimiter.middleware");
 
-router.post("/login", authLimiter, register);
-router.post("/register", authLimiter, login);
+router.post("/login", authLimiter, login);
+router.post("/register", authLimiter, register);
 
 module.exports = router;
