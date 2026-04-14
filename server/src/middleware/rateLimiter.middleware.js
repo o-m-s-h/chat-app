0b09cfe5f1bfd87596bc7122908c24808724ba2d
Omkar M Shewalkar <omkar.shewalkar936@gmail.com>
2026-04-14 12:34:31 +0530

    {RedisStore} added

--- server/src/middleware/rateLimiter.middleware.js
+++ server/src/middleware/rateLimiter.middleware.js
@@ -1,5 +1,6 @@
 const rateLimit = require("express-rate-limit");
-const RedisStore = require("rate-limit-redis");
+// const RedisStore = require("rate-limit-redis");
+const { RedisStore } = require("rate-limit-redis");
 const redis = require("../config/redis");
 
 // ==============================
